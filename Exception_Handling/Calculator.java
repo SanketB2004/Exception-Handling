cf3e530bdbcb563565bf47fd1de6d69aa99d80cf
SanketB2004 <bhosalesanket1507@gmail.com>
2026-07-31 16:57:24 +0530

    Commit

---
+++ Exception_Handling/Calculator.java
@@ -0,0 +1,28 @@
+package Exception_Handling;
+
+import java.util.Scanner;
+
+public class Calculator {
+    
+ public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("welcome to division claculator");
+    System.out.println("Pleasse enter your numbers");
+
+    int first = sc.nextInt();
+    int second = sc.nextInt();
+try {
+    int result = first / second;
+
+    System.out.println("Result : " + result);
+} catch (ArithmeticException exception)  {
+
+    System.out.println("Divide by zero , enter valid values");
+}
+
+
+
+ }   
+}

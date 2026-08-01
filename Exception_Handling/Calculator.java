43d11aba7e98d54a3108337c1754ae76a9928d9f
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-01 15:42:00 +0530

    Commit

--- Exception_Handling/Calculator.java
+++ Exception_Handling/Calculator.java
@@ -1,28 +1,61 @@
 package Exception_Handling;
 
+import java.io.EOFException;
 import java.util.Scanner;
 
 public class Calculator {
     
- public static void main(String[] args) {
+ public static void main(String[] args)throws Exception {
     
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("welcome to division claculator");
-    System.out.println("Pleasse enter your numbers");
+//     System.out.println("welcome to division claculator");
+//     System.out.println("Pleasse enter your numbers");
 
-    int first = sc.nextInt();
-    int second = sc.nextInt();
-try {
-    int result = first / second;
+//     int first = sc.nextInt();
+//     int second = sc.nextInt();
+// try {
+//     int result = first / second;
 
-    System.out.println("Result : " + result);
-} catch (ArithmeticException exception)  {
+//     System.out.println("Result : " + result);
+// // } catch (ArithmeticException th)  {
 
-    System.out.printf( "%s, enter valid values", exception.getMessage());
+// //     System.out.printf( "%s, enter valid values", th.getMessage());
+      
+
+// // } 
+// // }catch (Throwable ch)  {
+ 
+// //     // System.out.println( "Genral Exceptio");
+      
+// // throw new Exception( "Genral Exceptio");
+
+// // }
+// }catch (Throwable ch)  {
+ 
+//      System.out.println( "Genral Exceptio");
+      
+// throw ch;
+
+// }
+
+
+
+
+
+
+ checkAge(sc.nextInt());
+
+
+
+ 
 }
+static void checkAge(int age) {
 
+    if (age < 18) {
+        throw new RuntimeException("Age is less than 18");
+    }
 
-
- }   
+    System.out.println("You are eligible");
+}
 }

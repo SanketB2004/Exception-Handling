f022dc0aa8d61f771767da9d8e544e05293ea680
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-02 17:13:45 +0530

    Commit

--- reader.java
+++ reader.java
@@ -1,7 +1,24 @@
-public class reader {
-    public static void main(String[] args) {
-         String fileName = "Java-Course.txt";
+import java.io.FileReader;
+import java.io.IOException;
 
-         
+public class reader {
+
+    public static void main(String[] args) {
+
+        String fileName = "Java-Course.txt";
+
+        try (FileReader reader = new FileReader(fileName)) {
+
+            int data;
+
+            while ((data = reader.read()) != -1) {
+
+                System.out.print((char) data);
+            }
+
+        } catch (IOException e) {
+
+            System.out.println("Error Occurred: " + e.getMessage());
+        }
     }
 }

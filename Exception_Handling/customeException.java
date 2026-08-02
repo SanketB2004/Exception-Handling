1dcb96170b59857d10060263d9f16327a546c08c
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-02 17:19:02 +0530

    Commit

--- Exception_Handling/customeException.java
+++ Exception_Handling/customeException.java
@@ -1,5 +1,38 @@
 package Exception_Handling;
 
+// Custom Exception Class
+class InvalidAgeException extends Exception {
+
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
 public class customeException {
-    
+
+    static void checkAge(int age) throws InvalidAgeException {
+
+        if (age < 18) {
+
+            throw new InvalidAgeException(
+                "Age is less than 18"
+            );
+        }
+
+        System.out.println("You are eligible");
+    }
+
+    public static void main(String[] args) {
+
+        try {
+
+            checkAge(15);
+
+        } catch (InvalidAgeException e) {
+
+            System.out.println(
+                "Exception Occurred: " + e.getMessage()
+            );
+        }
+    }
 }

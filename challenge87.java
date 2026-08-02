7d8c5ffe42fb96509c2975a08390a26032860e25
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-02 16:23:18 +0530

    Coo

--- challenge87.java
+++ challenge87.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class challenge87 {
+    public class challenge87 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
---
+++ filehandling.java
@@ -0,0 +1,26 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class filehandling {
+
+    public static void main(String[] args) {
+
+        String fileName = "Java-Course.txt";
+
+        try {
+
+            FileWriter writer = new FileWriter(fileName);
+
+            writer.write("This is Java course");
+
+            writer.flush();
+            writer.close();
+
+            System.out.println("File Written Successfully");
+
+        } catch (IOException e) {
+
+            System.out.println("Error Occurred: " + e.getMessage());
+        }
+    }
+}

e50b8a125c1294418f7a634a5ddde71292e90700
SanketB2004 <bhosalesanket1507@gmail.com>
2026-08-02 15:59:02 +0530

    Coomit

--- challenge87.java
+++ challenge87.java
@@ -2,10 +2,28 @@ import java.util.Scanner;
 
 public class challenge87 {
     public static void main(String[] args) {
-        
+
         Scanner input = new Scanner(System.in);
-        
-        
+
+        System.out.println("Welcome to calculator");
+
+        System.out.println("Enter first number0");
+        int first = input.nextInt();
+        System.out.println("Enter second number0");
+        int second = input.nextInt();
+
+        try {
+            int result = first / second;
+            System.out.println(result);
+        } catch (ArithmeticException exception) {
+            if (exception.getMessage().equals("/ by zero")) {
+
+                System.out.println("Divide by zero occured");
+
+            } else {
+                throw exception;
+            }
+        }
 
     }
 }
